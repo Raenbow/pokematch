071c37028225ace6756b6e73a705b85dc5fb9215
delray <d.a.faskey@gmail.com>
2018-05-08 11:07:21 -0700

    started the three files

---
+++ main.js
@@ -0,0 +1,18 @@
+
+$(document).ready(initializeGame);
+
+
+var matches = 0;
+var attempts = 0;
+var accuracy = 0;
+var gamesPlayed = 0;
+
+function initializeGame(){
+
+}
+function displayStats(){
+
+}
+function resetStats(){
+
+}
