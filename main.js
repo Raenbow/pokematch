dfcdcf807d46cc40e9d400d34cfe79f951f08017
delray <d.a.faskey@gmail.com>
2018-05-08 23:29:39 -0700

    fixed card toggle, started bush toggle

--- main.js
+++ main.js
@@ -8,11 +8,31 @@ var accuracy = 0;
 var gamesPlayed = 0;
 
 function initializeGame(){
-
+    $(".cardTop").click(cardClicks);
+    bushToggle();
 }
 function displayStats(){
 
 }
 function resetStats(){
 
+}
+function cardClicks(){
+    if( $(this).hasClass("cardClick")){
+        $(this).removeClass("cardClick");
+        $(".bushStill").removeClass("hiddenBush");
+    } else {
+        $(this).addClass("cardClick");
+        $(".bushStill").addClass("hiddenBush");
+    }
+}
+function bushToggle(){
+    $(".bushStill").mouseenter(shakeyBush);
+    $(".bushStill").mouseout(stillBush);
+}
+function shakeyBush(){
+    $(this).attr("src", "images/shakeybush.gif");
+}
+function stillBush(){
+    $(this).attr("src", "images/stillbush.gif");
 }
