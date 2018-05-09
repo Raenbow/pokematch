0585290efd21490c9a3be6997db30998729ddabc
delray <d.a.faskey@gmail.com>
2018-05-09 14:33:41 -0700

    commented out bush function and started over to figure out functionality

--- main.js
+++ main.js
@@ -5,34 +5,62 @@ $(document).ready(initializeGame);
 var matches = 0;
 var attempts = 0;
 var accuracy = 0;
-var gamesPlayed = 0;
+
+var firstCardClicked = null;
+var secondCardClicked = null;
+var totalPossibleMatches = 2;
+var matchCounter = 0;
 
 function initializeGame(){
+    var gamesPlayed = 0;
     $(".cardTop").click(cardClicks);
-    bushToggle();
+//    bushToggle();
 }
-function displayStats(){
-
+function resetButton(){
+    gamesPlayed = gamesPlayed++;
+    return;
+}
+function displayStats(gamesPlayed, attempts, accuracy){
+    $(".gamesPlayed .value").text(gamesPlayed);
+    $(".attempts .value").text(attempts);
+    accuracy = (attempts - matches) / attempts * 100;
 }
 function resetStats(){
 
 }
 function cardClicks(){
-    if( $(this).hasClass("cardClick")){
-        $(this).removeClass("cardClick");
-        $(".bushStill").removeClass("hiddenBush");
+    if (firstCardClicked === null){
+        firstCardClicked = this;
+        console.log(firstCardClicked);
     } else {
-        $(this).addClass("cardClick");
-        $(".bushStill").addClass("hiddenBush");
+        secondCardClicked = this;
+        if (firstCardClicked === secondCardClicked){
+            matchCounter++;
+            firstCardClicked = null;
+            secondCardClicked = null;
+        }
+        if(matchCounter === totalPossibleMatches){
+            //display win!
+        } else {
+            ;
+        }
+        // if( $(this).hasClass("cardClick")){
+        //     $(this).removeClass("cardClick");
+        //     $(this).find(".bushStill").removeClass("hiddenBush");
+        // } else {
+        //     $(this).addClass("cardClick");
+        //     $(this).find(".bushStill").addClass("hiddenBush");
+        // }
     }
+
 }
-function bushToggle(){
-    $(".bushStill").mouseenter(shakeyBush);
-    $(".bushStill").mouseout(stillBush);
-}
-function shakeyBush(){
-    $(this).attr("src", "images/shakeybush.gif");
-}
-function stillBush(){
-    $(this).attr("src", "images/stillbush.gif");
-}
+// function bushToggle(){
+//     $(".bushStill").mouseenter(shakeyBush);
+//     $(".bushStill").mouseout(stillBush);
+// }
+// function shakeyBush(){
+//     $(this).attr("src", "images/shakeybush.gif");
+// }
+// function stillBush(){
+//     $(this).attr("src", "images/stillbush.gif");
+// }
