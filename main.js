b246c28bc4bedd28a680e9b797be11cd448bbadd
delray <d.a.faskey@gmail.com>
2018-08-12 16:34:16 -0700

    still working on easy difficulty

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ $(document).ready(initializeGame);
 
 var gameState = {
     difficulty: "regular",
+    cardArray: [],
+    imageArray: [],
     mute: true,
     stats: {
         attempts: 0,
@@ -14,16 +16,16 @@ var gameState = {
         gamesPlayed: 0,
         matchCounter: 0
     },
-    totalPossibleMatches: 9,
+    totalPossibleMatches: 12,
     firstCardClicked: null,
     secondCardClicked: null,
     thisCard1: null,
     thisCard2: null
 }
 
-var cardArray = [".cardImg1-1", ".cardImg1-2", ".cardImg1-3", ".cardImg1-4", ".cardImg1-5", ".cardImg1-6", ".cardImg2-1", ".cardImg2-2", ".cardImg2-3", ".cardImg2-4", ".cardImg2-5", ".cardImg2-6", ".cardImg3-1", ".cardImg3-2", ".cardImg3-3", ".cardImg3-4", ".cardImg3-5", ".cardImg3-6"];
-var pokemonArray1 = ["images/pmans1.jpg", "images/pmans2.jpg", "images/pmans3.jpg", "images/pmans4.jpg", "images/pmans5.jpg", "images/pmans6.jpg", "images/pmans7.jpg", "images/pmans8.jpg", "images/pmans9.jpg"];
-var pokemonArray2 = ["images/pmans1.jpg", "images/pmans2.jpg", "images/pmans3.jpg", "images/pmans4.jpg", "images/pmans5.jpg", "images/pmans6.jpg", "images/pmans7.jpg", "images/pmans8.jpg", "images/pmans9.jpg"];
+// var cardArray = [".cardImg1-1", ".cardImg1-2", ".cardImg1-3", ".cardImg1-4", ".cardImg1-5", ".cardImg1-6", ".cardImg2-1", ".cardImg2-2", ".cardImg2-3", ".cardImg2-4", ".cardImg2-5", ".cardImg2-6", ".cardImg3-1", ".cardImg3-2", ".cardImg3-3", ".cardImg3-4", ".cardImg3-5", ".cardImg3-6"];
+// var pokemon12Array = ["images/pokemon12/image1.jpg", "images/pokemon12/image2.jpg", "images/pokemon12/image3.jpg", "images/pokemon12/image4.jpg", "images/pokemon12/image5.jpg", "images/pokemon12/image6.jpg", "images/pokemon12/image7.jpg", "images/pokemon12/image8.jpg", "images/pokemon12/image9.jpg", "images/pokemon12/image10.jpg", "images/pokemon12/image11.jpg", "images/pokemon12/image12.jpg"];
+// var pokemonArray2 = ["images/image1.jpg", "images/image2.jpg", "images/image3.jpg", "images/image4.jpg", "images/image5.jpg", "images/image6.jpg", "images/image7.jpg", "images/image8.jpg", "images/image9.jpg"];
 
 //-------------------------------------------------------------------------------------------------------
 
@@ -52,6 +54,11 @@ function attachClickHandler(){
     $(".winModal").on("click", winModalClose);
     $(".soundToggleButton").on("click", toggleSounds);
     $(".pokedexOpenButton").on("click", pokedexExpand);
+    $(".settingsButton").on("click", settingsMenuOpen);
+    $(".statsButton").on("click", statsMenuOpen);
+    $(".easy").on("click", difficultySwitch_Easy);
+    $(".reg").on("click", difficultySwitch_Reg);
+    $(".hard").on("click", difficultySwitch_Hard);
 }
 function resizeTransitionStop(){
     $(".pokedex").css("transition-property", "none");
@@ -77,6 +84,20 @@ function pokedexExpand(){
         $(".pokedexOpenButton").removeClass("switched");
     }
 }
+function settingsMenuOpen(){
+    console.log("Ya clickety clacked that settings button man!");
+    $(".screenMiddle").css("display", "none");
+    $(".settingsDisplay").css("display","flex");
+    $(".settingsButton").css("display", "none");
+    $(".statsButton").css("display", "unset");
+}
+function statsMenuOpen(){
+    console.log("Ya clickety clacked that STATS button man!");
+    $(".screenMiddle").css("display", "flex");
+    $(".settingsDisplay").css("display","none");
+    $(".settingsButton").css("display", "unset");
+    $(".statsButton").css("display", "none");
+}
 function toggleSounds(){
     if ($(".soundToggleButton").hasClass("soundoff")){
         $(".soundToggleButton").removeClass("soundoff").addClass("soundon");
@@ -89,6 +110,36 @@ function toggleSounds(){
         $(".soundToggleIcon").attr("src", "./images/soundoff.png");
         return gameState.mute = true;
     }
+}
+function difficultySwitch_Easy(){
+    gameState.difficulty = "easy";
+    gameState.totalPossibleMatches = 8;
+    $(".gameBoard").empty();
+    renderGameBoard();
+    shuffle();
+    $(".card").addClass("easySize").removeClass("hardSize, regSize");
+    $(".card").on("click", cardClicks);
+    console.log("Difficulty = Easy");
+}
+function difficultySwitch_Reg(){
+    gameState.difficulty = "regular";
+    gameState.totalPossibleMatches = 12;
+    $(".gameBoard").empty();
+    renderGameBoard();
+    shuffle();
+    $(".card").addClass("regSize").removeClass("hardSize, easySize");
+    $(".card").on("click", cardClicks);
+    console.log("Difficulty = Regular");
+}
+function difficultySwitch_Hard(){
+    gameState.difficulty = "hard";
+    gameState.totalPossibleMatches = 18;
+    $(".gameBoard").empty();
+    renderGameBoard();
+    shuffle();
+    $(".card").addClass("hardSize").removeClass("regSize, easySize");
+    $(".card").on("click", cardClicks);
+    console.log("Difficulty = Hard");
 }
 function resetButton(){
     gameState.stats.gamesPlayed++;
@@ -116,15 +167,60 @@ function displayStats(){
     $(".accuracy .value").text(Math.floor(gameState.stats.accuracy) + "%");
     return gameState.stats;
 }
+function displayDifficulty(){
+    if (gameState.difficulty === "easy"){
+        $(".easy > i").removeClass("far fa-star").addClass("fas fa-star");
+        $(".reg > i, .hard > i").removeClass("fas fa-star").addClass("far fa-star");
+    } else if (gameState.difficulty === "regular"){
+        $(".reg > i").removeClass("far fa-star").addClass("fas fa-star");
+        $(".easy > i, .hard > i").removeClass("fas fa-star").addClass("far fa-star");
+    } else if (gameState.difficulty === "hard"){
+        $(".hard > i").removeClass("far fa-star").addClass("fas fa-star");
+        $(".easy > i, .reg > i").removeClass("fas fa-star").addClass("far fa-star");
+    }
+}
 //-------------------------------------------------------------------------------------------------------
 
 function renderGameBoard(){
-    if (gameState.difficulty === "regular"){
-        createRegularBoard()
+    if (gameState.difficulty === "easy"){
+        createEasyBoard();
+        displayDifficulty();
+    } else if (gameState.difficulty === "regular"){
+        createRegularBoard();
+        displayDifficulty();
+    } else if (gameState.difficulty === "hard"){
+        createHardBoard();
+        displayDifficulty();
+    }
+}
+function createEasyBoard(){
+    for( numOfRows=1; numOfRows<=4; numOfRows++ ){
+        $(".gameBoard").append(
+            $("<div>", {"class": `row${numOfRows}`})
+        );
+
+        for( cardsPerRow=1; cardsPerRow<=4; cardsPerRow++ ){
+            $(`.row${numOfRows}`)
+            .append(
+                $("<div>", {"class": "card"})
+                .append(
+                    $("<div>", {"class": "cardBottom"})
+                    .append(
+                        $("<img>", {"src": "images/pokemon25/image1.jpg", "class": `cardImg${numOfRows}-${cardsPerRow}`})
+                    ),
+                    $("<div>", {"class": "cardTop"})
+                    .append(
+                        $("<img>", {"src": "images/greenbackground2.jpg"})
+                    )
+                )
+            );
+
+            gameState.cardArray.push(`.cardImg${numOfRows}-${cardsPerRow}`);
+        }
     }
 }
 function createRegularBoard(){
-    for( numOfRows=1; numOfRows<=3; numOfRows++ ){
+    for( numOfRows=1; numOfRows<=4; numOfRows++ ){
         $(".gameBoard").append(
             $("<div>", {"class": `row${numOfRows}`})
         );
@@ -136,7 +232,7 @@ function createRegularBoard(){
                 .append(
                     $("<div>", {"class": "cardBottom"})
                     .append(
-                        $("<img>", {"src": "images/poke1-1.png", "class": `cardImg${numOfRows}-${cardsPerRow}`})
+                        $("<img>", {"src": "images/pokemon12/image1.jpg", "class": `cardImg${numOfRows}-${cardsPerRow}`})
                     ),
                     $("<div>", {"class": "cardTop"})
                     .append(
@@ -144,19 +240,62 @@ function createRegularBoard(){
                     )
                 )
             );
+
+            gameState.cardArray.push(`.cardImg${numOfRows}-${cardsPerRow}`);
         }
     }
 }
-function shuffle(array){
-    var pokemonArrayx18 = pokemonArray1.concat(pokemonArray2);
-    var randomPokemon = [];
-    while (pokemonArrayx18.length > 0){
-        var randomIndex = Math.floor(Math.random() * pokemonArrayx18.length);
-        randomPokemon.push(pokemonArrayx18[randomIndex]);
-        pokemonArrayx18.splice(randomIndex, 1);
+function createHardBoard(){
+    for( numOfRows=1; numOfRows<=4; numOfRows++ ){
+        $(".gameBoard").append(
+            $("<div>", {"class": `row${numOfRows}`})
+        );
+
+        for( cardsPerRow=1; cardsPerRow<=9; cardsPerRow++ ){
+            $(`.row${numOfRows}`)
+            .append(
+                $("<div>", {"class": "card"})
+                .append(
+                    $("<div>", {"class": "cardBottom"})
+                    .append(
+                        $("<img>", {"src": "images/pokemon25/image1.jpg", "class": `cardImg${numOfRows}-${cardsPerRow}`})
+                    ),
+                    $("<div>", {"class": "cardTop"})
+                    .append(
+                        $("<img>", {"src": "images/greenbackground2.jpg"})
+                    )
+                )
+            );
+
+            gameState.cardArray.push(`.cardImg${numOfRows}-${cardsPerRow}`);
+        }
     }
-    for(i=0;i<=18;i++){
-        $(cardArray[i]).attr('src', randomPokemon[i]);
+}
+function createImageArray(){
+    if (gameState.difficulty === "regular"){
+        for (doubleIt=1; doubleIt<=2; doubleIt++){
+            for (pokemonImgNum=1; pokemonImgNum<=12; pokemonImgNum++){
+                gameState.imageArray.push(`images/pokemon12/image${pokemonImgNum}.png`)
+            }
+        }
+    } else if (gameState.difficulty === "hard"){
+        for (doubleIt=1; doubleIt<=2; doubleIt++){
+            for (pokemonImgNum=1; pokemonImgNum<=25; pokemonImgNum++){
+                gameState.imageArray.push(`images/pokemon25/image${pokemonImgNum}.jpg`)
+            }
+        }
+    }
+}
+function shuffle(){
+    createImageArray();
+    var randomPokemon = [];
+    while (gameState.imageArray.length > 0){
+        var randomIndex = Math.floor(Math.random() * gameState.imageArray.length);
+        randomPokemon.push(gameState.imageArray[randomIndex]);
+        gameState.imageArray.splice(randomIndex, 1);
+    }
+    for(i=0;i<=randomPokemon.length;i++){
+        $(gameState.cardArray[i]).attr('src', randomPokemon[i]);
     }
 }
 function cardClicks(){
